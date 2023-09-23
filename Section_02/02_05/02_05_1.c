9358d96a26bfeae981421fde3fa6ff91d5378d0b
Luverduck <hyekko92@gmail.com>
2023-09-23 19:01:12 +0900

    2.5 변수를 선언하는 방법

---
+++ Section_02/02_05/02_05_1.c
@@ -0,0 +1,22 @@
+// 2.5 변수를 선언하는 방법
+
+int main(void)
+{
+	// 변수의 선언 (declaration)
+	// 정수 하나를 저장할 공간을 확보했으며 (allocation)
+	// 해당 변수를 통해 해당 공간에 접근할 수 있다.
+	int x;
+	int y;
+	int z;
+
+	// 변수에 값을 대입 (assignment) 또는 초기화(initialization)
+	// 변수 x에 1을 대입
+	// 변수 y에 2을 대입
+	x = 1;
+	y = 2;
+	
+	// 변수에 연산(operation)의 결과를 대입
+	z = x + y;
+
+	return 0;
+}
