4ec380af91763478d7b74c83320e5d0e6be7ef7d
Luverduck <hyekko92@gmail.com>
2023-09-23 19:13:27 +0900

    2.6 주석 작성

---
+++ Section_02/02_06/02_06.c
@@ -0,0 +1,18 @@
+// 2.6 주석 작성
+
+int main(void)
+{
+	// 주석의 종류
+	// 1. 한 줄 주석
+	int x = 1;
+	int y = 2;
+	// int z = x + y;
+
+	// 2. 영역 주석
+	/*int a = 1;
+	int b = 2;*/
+
+	int c = a + b;
+
+	return 0;
+}
