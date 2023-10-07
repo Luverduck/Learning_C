755929059e8b33b2fffbd1d1578e256f9758d572
Luverduck <hyekko92@gmail.com>
2023-10-07 16:53:43 +0900

    3.13 불형

--- Section_03/03_13/03_13_1.c
+++ Section_03/03_13/03_13_1.c
@@ -1,29 +1,44 @@
-// 3.13 불리언형 (Boolean types)
+// 3.13 불형
 /*
-	boolean 
+	불형(_Bool)
+	- C99 이후 추가된 논리 자료형
+	- 서식 지정자 없음
+	
+	_Bool (표준형)
+	숫자 0 또는 1로 값을 표현 (0은 false, 1은 true에 대응)
+
+	bool (stdbool.h를 포함할 경우)
+	숫자 0 또는 1로 값을 표현 (0은 false, 1은 true에 대응)
+	키워드 false, true로 값을 표현 (출력시 false / true의 값을 숫자로 표시)
 */
 
 #include <stdio.h>
 #include <stdbool.h> // 불리언형에 대한 헤더 파일
 int main(void)
 {
-	// 불리언형의 크기
-	printf("%u\n", sizeof(_Bool)); // 1[Byte]
+	// 표준형
+	_Bool boolean;
 
-	// 불리언형
-	_Bool b1;
-	b1 = 0; // false
-	b1 = 1; // true
+	// 가질 수 있는 값
+	boolean = 1; // true
+	boolean = 0; // false
 
-	// 불리언형의 서식 지정자는 따로 없다.
-	printf("%d\n", b1); // 결과 : 1
+	// 크기 : 1[Byte]
+	printf("%u\n", sizeof(_Bool)); // 결과 : 1
 
-	// stdbool.h에 포함된 불리언형
-	bool b2, b3;
-	b2 = true;
-	b3 = false;
+	// bool
+	bool boolean2, boolean3;
 
-	printf("%d %d", b2, b3); // 결과 : 1 0
+	// 가질 수 있는 값
+	// 1) 정수
+	boolean2 = 1; // true
+	boolean2 = 0; // false
+	// 2) 키워드
+	boolean3 = true; // true
+	boolean3 = false; // false
+
+	// 출력값
+	printf("%d %d", boolean2, boolean3); // 결과 : 0 0
 
 	return 0;
 }
