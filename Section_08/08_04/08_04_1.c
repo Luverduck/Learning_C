5a2c591395343781517d1c46f6a24ecd217beafd
Luverduck <hyekko92@gmail.com>
2023-11-18 16:42:14 +0900

    8.4 사용자 인터페이스는 친절하게

--- Section_08/08_04/08_04_1.c
+++ Section_08/08_04/08_04_1.c
@@ -5,22 +5,28 @@ int main(void)
 {
 	// TO DO : 'y'를 입력하면 카운트 +1, 'n'을 입력하면 중지하는 프로그램
 	int count = 0;
+	char ch;
 
 	while (1) {
+		// 안내 문자 출력
 		printf("Current count is %d. Continue? (y/n)\n", count);
 		
-		if (getchar() == 'n') {
+		// 입력
+		ch = getchar();
+
+		// 분기 처리
+		if (ch == 'n') {
 			break;
 		}
-		else if (getchar() == 'y') {
+		else if (ch == 'y') {
 			count++;
 		}
 		else {
 			printf("Please input y or n.\n");
 		}
 		
-		// 입력 버퍼에서 꺼낸 문자가 입력의 끝(\n)일 때까지 반복 (입력 버퍼 비우기)
-		while (getchar() == '\n') {
+		// 입력 버퍼 비우기 (입력 버퍼에서 꺼낸 문자가 입력의 끝(\n)일 때까지 반복)
+		while ((ch = getchar()) != '\n') {
 			continue;
 		}
 	}
