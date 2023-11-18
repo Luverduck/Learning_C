a4a9946d7d9babae220a47c3b27ac2f2137e6785
Luverduck <hyekko92@gmail.com>
2023-11-18 16:35:38 +0900

    8.4 사용자 인터페이스는 친절하게

---
+++ Section_08/08_04/08_04_1.c
@@ -0,0 +1,29 @@
+// 8.4 사용자 인터페이스는 친절하게
+
+#include <stdio.h>
+int main(void)
+{
+	// TO DO : 'y'를 입력하면 카운트 +1, 'n'을 입력하면 중지하는 프로그램
+	int count = 0;
+
+	while (1) {
+		printf("Current count is %d. Continue? (y/n)\n", count);
+		
+		if (getchar() == 'n') {
+			break;
+		}
+		else if (getchar() == 'y') {
+			count++;
+		}
+		else {
+			printf("Please input y or n.\n");
+		}
+		
+		// 입력 버퍼에서 꺼낸 문자가 입력의 끝(\n)일 때까지 반복 (입력 버퍼 비우기)
+		while (getchar() == '\n') {
+			continue;
+		}
+	}
+
+	return 0;
+}
