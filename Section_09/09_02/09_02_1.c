7e62fc6b58ac98e87a783b3186e563aba853d7db
Luverduck <hyekko92@gmail.com>
2023-11-25 23:45:58 +0900

    9.2 함수의 프로토타입

---
+++ Section_09/09_02/09_02_1.c
@@ -0,0 +1,45 @@
+// 9.2 함수의 프로토타입
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h> // strlen()
+
+#define WIDTH 40
+#define NAME "Hyun-Yong Eom"
+#define ADDRESS "Seoul, Korea"
+
+// 함수 원형 (prototype)
+void print_multiple_char(char ch, int n, bool new_line);
+void print_centered_str(char str[]);
+
+
+int main(void)
+{
+	print_multiple_char('*', WIDTH, true);
+
+	print_centered_str(NAME);
+
+	print_centered_str(ADDRESS);
+
+	print_multiple_char('*', WIDTH, false);
+
+	return 0;
+}
+
+// 함수 정의 (definition)
+void print_multiple_char(char ch, int n, bool new_line) {
+	for (int i = 0; i < n; ++i) {
+		printf("%c", ch);
+	}
+	if (new_line) {
+		printf("\n");
+	}
+};
+
+void print_centered_str(char str[]) {
+	int n_blank;
+	n_blank = (WIDTH - strlen(str)) / 2;
+	print_multiple_char(' ', n_blank, false);
+	printf("%s\n", str);
+};
