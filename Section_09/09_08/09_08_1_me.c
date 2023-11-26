4896603332bc6167542b75f9177ee52cef354239
Luverduck <hyekko92@gmail.com>
2023-11-26 20:09:03 +0900

    9.8 팩토리얼 예제

---
+++ Section_09/09_08/09_08_1_me.c
@@ -0,0 +1,50 @@
+// 9.8 팩토리얼 예제
+/*
+	factorial
+	0! = 1
+	3! = 3 * 2 * 1
+	5! = 5 * 4! 
+	   = 5 * 4 * 3! 
+	   = 5 * 4 * 3 * 2! 
+	   = 5 * 4 * 3 * 2 * 1! 
+	   = 5 * 4 * 3 * 2 * 1 * 0!
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// 반복문으로 구현한 팩토리얼 함수
+long loop_factorial(int n);
+
+// 재귀 호출로 구현한 팩토리얼 함수
+long recursive_factorial(int n);
+
+int main(void)
+{
+	int n = 5;
+
+	printf("%d! = %ld\n", n, loop_factorial(5));
+	printf("\n");
+	printf("%d! = %ld\n", n, recursive_factorial(5));
+
+	return 0;
+}
+
+// 반복문으로 구현한 팩토리얼 함수
+long loop_factorial(int n)
+{
+	int f = 1;
+	for (int i = n; i > 0; --i) {
+		f *= i;
+	}
+	return f;
+};
+
+// 재귀 호출로 구현한 팩토리얼 함수
+long recursive_factorial(int n) 
+{
+	if (n > 1)
+		return n * recursive_factorial(n - 1);
+	else
+		return 1;
+};
