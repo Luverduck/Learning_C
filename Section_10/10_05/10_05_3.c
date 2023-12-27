a286e5aa01b09683c6908748e78f094f7b6e12b0
Luverduck <hyekko92@gmail.com>
2023-12-27 17:31:10 +0900

    10.5 2차원 배열과 메모리

--- Section_10/10_05/10_05_3.c
+++ Section_10/10_05/10_05_3.c
@@ -10,9 +10,9 @@ int main(void)
 	int arr[2][3] = { {0, 1, 2}, {3, 4, 5} };
 
 	// 2차원 배열의 모든 요소 출력
-	for (int j = 0; j < sizeof(arr) / sizeof(arr[0]); ++j) {
-		for (int i = 0; i < sizeof(arr[0]) / sizeof(int); ++i) {
-			printf("arr[%d][%d] = %d, &arr[%d][%d] = %p\n", j, i, arr[j][i], j, i, &arr[j][i]);
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i) {
+		for (int j = 0; j < sizeof(arr[0]) / sizeof(int); ++j) {
+			printf("arr[%d][%d] = %d, &arr[%d][%d] = %p\n", i, j, arr[i][j], j, i, &arr[i][j]);
 		};
 	};
 
