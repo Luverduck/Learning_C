61add4b7bcb676ca64099d4e11798910c088a25d
Luverduck <hyekko92@gmail.com>
2024-01-02 22:08:18 +0900

    12.5 자동 변수

--- Section_12/12_05/12_05_1.c
+++ Section_12/12_05/12_05_1.c
@@ -17,22 +17,38 @@ int main(void)
 	// - auto 키워드 생략
 	int y = 0;
 
-	// 2. 블록 안에서 블록 밖에 있는 자동 변수의 식별자를 사용할 수 있다.
-	int i = 1;
+	// 2. 자동 변수의 영역 안에서만 자동 변수의 식별자를 사용할 수 있다.
+	// - 자동 변수의 영역 안
+	{ // 자동 변수 i의 영역 [START]
+		auto int i = 1;
+		{
+			// 자동 변수의 영역 내에 있는 새로운 블록에서도 자동 변수의 식별자 사용 가능
+			printf("i = %d\n", i);
+		}
+	} // 자동 변수 i의 영역 [END]
 
+	// - 자동 변수의 영역 밖
 	{
-		printf("i = %d\n", i);
+		{ // 자동 변수 j의 영역 [START]
+			int j = 5;
+		} // 자동 변수 j의 영역 [END]
+		// 자동 변수의 영역 밖에서는 자동 변수의 식별자 사용 불가능
+		// printf("j = %d\n", j);
 	}
 
 	// 3. 블록 안과 밖에 동일한 식별자가 존재할 때 블록 안에서 블록 밖의 식별자는 숨김 처리 된다. (name hiding)
-	int j = 2;
+	int k = 2;
 
-	printf("j %lld\n", (long long)&j);
+	printf("k %lld\n", (long long)&k);    // 결과 : 856600212932
 
 	{
 		int j = 10; // name hiding
-		printf("j %lld\n", (long long)&j);
+		printf("k %lld\n", (long long)&k);  // 결과 : 856600212964
 	}
+
+	// 4. 블록이 없을 경우 자동 변수의 영역은 문장(Statement)의 시작부터 문장의 끝(;)까지가 된다.
+	for (int l = 0; l < 10; ++l) 
+		printf("%d\n", l);
 
 	return 0;
 }
