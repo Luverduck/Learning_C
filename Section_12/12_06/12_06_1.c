f2d201947cacc63d6e3430b80f5577160dd2bc95
Luverduck <hyekko92@gmail.com>
2024-01-02 22:17:40 +0900

    12.6 레지스터 변수

---
+++ Section_12/12_06/12_06_1.c
@@ -0,0 +1,16 @@
+// 12.6 레지스터 변수
+/*
+	레지스터 변수 (Register Variable)
+	Register Storage Class에 속하는 변수
+	- 지속 시간 : automatic
+	- 변수 영역 : block
+	- 연결 상태 : none
+	- 저장 위치 : 레지스터(Register) 또는 스택(Stack)
+*/
+
+#include <stdio.h>
+int main(void)
+{
+
+	return 0;
+}
