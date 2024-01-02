c8077bf80527a6302cfc9d8c75796b7f415b502a
Luverduck <hyekko92@gmail.com>
2024-01-02 20:49:37 +0900

    12.4 저장 공간의 다섯 가지 분류

---
+++ Section_12/12_04/12_04_1.c
@@ -0,0 +1,17 @@
+// 12.4 저장 공간의 다섯 가지 분류
+/*
+	저장 공간 분류 (Storage Class)
+	1) non-static
+	- automatic, register
+	
+	2) static
+	- static with external linkage
+	- static with internal linkage
+	- static with no linkage
+*/
+#include <stdio.h>
+int main(void)
+{
+
+	return 0;
+}
