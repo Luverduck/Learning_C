5379a6b62da0ebea34b3a6c89369d2e6545bde08
Luverduck <hyekko92@gmail.com>
2024-01-08 03:35:02 +0900

    13.1 파일 입출력의 작동 원리

---
+++ Section_13/13_01/13_01_1.c
@@ -0,0 +1,30 @@
+// 13.1 파일 입출력의 작동 원리
+/*
+	표준 스트림 (Standard Stream)
+	1) 표준 입력 스트림 (Standard Input Stream, stdin)
+		- 프로그램이 입력 데이터를 읽는 스트림
+	2) 준 출력 스트림 (Standard Output Stream, stdout)
+		- 프로그램이 출력 데이터를 쓰는 스트림
+	3) 표준 에러 스트림 (Standard Error Stream, stderr)
+		- 프로그램이 오류 메시지를 쓰는 스트림 (표준 출력과는 별개의 스트림)
+
+	표준 스트림의 리디렉션 (Redirection)
+	표준 스트림을 사용자 지정 위치로 변경
+	- 표준 입력 스트림을 키보드 입력이 아닌 파일 입력으로 바꿀 수 있다.
+	- 표준 출력 스트림을 화면 출력이 아닌 파일 출력으로 바꿀 수 있다.
+
+	파일 입출력에 사용되는 파일의 종류
+	1) 바이너리 파일 (Binary File)
+		텍스트가 아닌 것(Non-Text)으로 변환될 수 있는 이진 코드(Binary Code)가 저장된 파일
+		- 실행 파일(.exe), 이미지 파일(.png), 비디오 파일(.avi) …
+	2) 텍스트 파일 (Text File)
+		텍스트(Text)로 변환될 수 있는 이진 코드(Binary Code)가 저장된 파일
+		- 텍스트 파일(.txt), HTML 파일(.html)
+*/
+
+#include <stdio.h>
+int main(void)
+{
+
+	return 0;
+}
